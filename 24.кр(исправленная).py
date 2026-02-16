75d4d50fcccd2646693d3098798842b59d494581
dmitriuenotov2008-cmyk <dmitriuenotov2008@gmail.com>
2026-02-16 23:03:08 +0300

    Add files via upload

---
+++ 24.кр(исправленная).py
@@ -0,0 +1,11 @@
+s=open('24(564645).txt').readline()
+t=0
+a=0
+maxline=[]
+for x in range(1,len(s)):
+    if (s[x-1]=='C' or s[x-1]=='D' or s[x-1]=='F') and (s[x]=='A' or s[x]=='O'):
+        t+=1
+        if t<a:
+            t=a
+            maxline.append(t)
+print(max(maxline))
